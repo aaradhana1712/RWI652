251710583899c4e9ca558cf17717fe2a28261685
Aaradhana  Mewade <117636129+aaradhana1712@users.noreply.github.com>
2024-05-23 17:36:46 +0530

    static block
    3rd_day

---
+++ RWI_programs/src/programs/first.java
@@ -0,0 +1,16 @@
+package programs;
+
+public class first {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Main Method");
+
+	    }
+	static {
+		System.out.println("Static block");
+	    }
+
+	}
+
+
