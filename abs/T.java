0f178e1b5ef613804c4a99dcf3c063cf9c830630
Aaradhana  Mewade <117636129+aaradhana1712@users.noreply.github.com>
2024-05-30 20:35:11 +0530

    Abstact

---
+++ abs/T.java
@@ -0,0 +1,15 @@
+package com.abs;
+
+public class T {
+
+	public static void main(String[] args) {
+		Bus b = new Bus();
+		  System.out.println("Bus class");
+		  System.out.println(b.m1());
+		  Auto a = new Auto();
+		  System.out.println("Auto class");
+		  System.out.println(a.m1());
+		      
+	}
+
+}
---
+++ abs/Vehicle.java
@@ -0,0 +1,5 @@
+package com.abs;
+
+public abstract class Vehicle {
+	public abstract int m1();
+}
