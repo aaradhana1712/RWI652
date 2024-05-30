6ea343f668a3bbd20acd923ffe3693d318185ac3
Aaradhana  Mewade <117636129+aaradhana1712@users.noreply.github.com>
2024-05-30 20:32:28 +0530

    Access modifier's

---
+++ pack3/A.java
@@ -0,0 +1,13 @@
+package pack3;
+
+ class A {
+
+}
+ // default class are only within the class
+ class Test{
+	 public static void main(String[] args) {
+		 A a = new A();
+		 System.out.println("hello");
+	 }
+	  
+ }
