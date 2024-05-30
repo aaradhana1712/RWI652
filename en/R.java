e8affac57da542d18058913b91fd595cd72701f7
Aaradhana  Mewade <117636129+aaradhana1712@users.noreply.github.com>
2024-05-30 20:37:42 +0530

    encapsulation

---
+++ en/R.java
@@ -0,0 +1,23 @@
+package com.en;
+
+public class R {
+
+	private static final String LG = null;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Ref rf = new Ref();
+	   System.out.println(rf.getName());
+	   System.out.println(rf.getProductId());
+	   rf.setName(LG);
+	   rf.setProductId(111);
+	  
+	System.out.println(rf.getName());
+	System.out.println(rf.getProductId());
+	   
+	   
+		
+
+	}
+
+}
---
+++ en/Ref.java
@@ -0,0 +1,20 @@
+package com.en;
+
+public class Ref {
+  private String name;
+  private int productId;
+
+  
+  public String getName() {
+	  return name;
+  }
+  public void setName(String name) {
+	this.name = name;  
+  }
+  public int getProductId() {
+	  return productId;
+  }
+  public void setProductId(int productId) {
+	  this.productId = productId;
+  }
+  }
